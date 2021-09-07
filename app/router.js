3cbc412e5105a5ef7886f3a92bfe34b1e4a18b68
zhaoyunjava1@gmail.com <czzKbg7yaZhwdQx>
2021-09-07 16:41:40 +0800

    x

---
+++ app/router.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+	const {
+		router,
+		controller
+	} = app;
+	router.get('/createOrder', controller.payment.createOrder);
+	router.get('/orderCallBack', controller.payment.orderCallBack);
+};
