05fcbea69073cd6faaccba6df25661e8b5d546ef
zhaoyunjava1@gmail.com <czzKbg7yaZhwdQx>
2021-09-08 01:13:15 +0800

    x

--- config/config.default.js
+++ config/config.default.js
@@ -6,36 +6,36 @@
  * @param {Egg.EggAppInfo} appInfo app info
  */
 module.exports = appInfo => {
-	/**
-	 * built-in config
-	 * @type {Egg.EggAppConfig}
-	 **/
-	const config = exports = {};
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
 
-	// use for cookie sign key, should change to your own and keep security
-	config.keys = appInfo.name + '_1630914131613_4865';
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1630914131613_4865';
 
-	// add your middleware config here
-	config.middleware = [];
+  // add your middleware config here
+  config.middleware = [];
 
-	// add your user config here
-	const userConfig = {
-		// myAppName: 'egg',
-	};
-	config.security = {
-		csrf: {
-			enable: false,
-			ignoreJSON: true,
-		},
-		domainWhiteList: ['*'],
-	};
-	config.cors = {
-		origin: '*', // 匹配规则  域名+端口  *则为全匹配
-		allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
-	};
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true,
+    },
+    domainWhiteList: [ '*' ],
+  };
+  config.cors = {
+    origin: '*', // 匹配规则  域名+端口  *则为全匹配
+    allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH',
+  };
 
-	return {
-		...config,
-		...userConfig,
-	};
+  return {
+    ...config,
+    ...userConfig,
+  };
 };
--- config/plugin.js
+++ config/plugin.js
@@ -3,9 +3,10 @@
 /** @type Egg.EggPlugin */
 module.exports = {
   // had enabled by egg
-  // static: {
+  // exports.assets = {
   //   enable: true,
-  // },
+  //   package: 'egg-view-assets',
+  // }
   cors: {
     enable: true,
     package: 'egg-cors',
