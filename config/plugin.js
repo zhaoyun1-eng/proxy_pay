55f5458669428f796ed479ec5cbdea6cd08040b0
zhaoyunjava1@gmail.com <czzKbg7yaZhwdQx>
2021-09-08 00:50:34 +0800

    x

--- config/plugin.js
+++ config/plugin.js
@@ -3,9 +3,9 @@
 /** @type Egg.EggPlugin */
 module.exports = {
   // had enabled by egg
-  static: {
-    enable: true,
-  },
+  // static: {
+  //   enable: true,
+  // },
   cors: {
     enable: true,
     package: 'egg-cors',
